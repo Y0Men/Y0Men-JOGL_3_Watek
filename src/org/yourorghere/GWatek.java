a70d81a65e6650369274c136a57abad2a02bfb60
student <student@240-03.INFORMATYKA.local>
2016-11-03 14:53:01 +0100

    Lab3 Zad+ - Oswietlenie walcy

--- src/org/yourorghere/GWatek.java
+++ src/org/yourorghere/GWatek.java
@@ -30,6 +30,8 @@ public class GWatek implements GLEventListener {
     public static float diffuseLight[] = {0.7f, 0.7f, 0.7f, 1.0f};//?wiat?o rozproszone
     public static float specular[] = {1.0f, 1.0f, 1.0f, 1.0f}; //?wiat?o odbite
     public static float lightPos[] = {0.0f, 150.0f, 150.0f, 1.0f};//pozycja ?wiat?a
+    public static float lightPos2[] = {0.0f, 150.0f, -150.0f, 1.0f};
+    public static float direction[]={0,0,0};
 
     public static void main(String[] args) {
         Frame frame = new Frame("Simple JOGL Application");
@@ -136,7 +138,15 @@ public class GWatek implements GLEventListener {
         gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuseLight, 0); //œwiat³o rozproszone
         gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, specular, 0); //œwiat³o odbite
         gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, lightPos, 0); //pozycja œwiat³a
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPOT_DIRECTION, direction ,0);
         gl.glEnable(GL.GL_LIGHT0); //uaktywnienie Ÿród³a œwiat³a nr. 0
+
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_AMBIENT, ambientLight, 0);
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_DIFFUSE, diffuseLight, 0); 
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPECULAR, specular, 0); 
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_POSITION, lightPos2, 0); 
+        gl.glEnable(GL.GL_LIGHT1);
+
         gl.glEnable(GL.GL_COLOR_MATERIAL); //uaktywnienie œledzenia kolorów
         //kolory bêd¹ ustalane za pomoc¹ glColor
         gl.glColorMaterial(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE);
@@ -288,28 +298,77 @@ public class GWatek implements GLEventListener {
         gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuseLight, 0); //œwiat³o rozproszone
         gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, specular, 0); //œwiat³o odbite
 
-           float x,y,kat;
-gl.glBegin(GL.GL_QUAD_STRIP);
+        float x, y, kat;
+        gl.glBegin(GL.GL_QUAD_STRIP);
 //gl.glVertex3f(0.0f,0.0f,-6.0f); //?rodek
-gl.glColor3f(1.0f,1.0f,0.0f);
-for(kat = 0.0f; kat < (2.0f*Math.PI);
-kat+=(Math.PI/32.0f))
-{
-x = 1.0f*(float)Math.sin(kat);
-y = 1.0f*(float)Math.cos(kat);
-gl.glNormal3f(x, 0.0f, 0.0f);
-gl.glVertex3f(x, 2.0f, y);
-gl.glVertex3f(x, -2.0f, y);//kolejne punkty
-}
-gl.glEnd();
+        gl.glColor3f(1.0f, 1.0f, 0.0f);
+        for (kat = 0.0f; kat < (2.0f * Math.PI);
+                kat += (Math.PI / 32.0f)) {
+            x = 1.0f * (float) Math.sin(kat);
+            y = 1.0f * (float) Math.cos(kat);
+            gl.glNormal3f(x, 0.0f, 0.0f);
+            gl.glVertex3f(x, 2.0f, y);
+            gl.glVertex3f(x, -2.0f, y);//kolejne punkty
+        }
+        gl.glEnd();
 
+        gl.glBegin(GL.GL_TRIANGLE_FAN);
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+        createShape(gl, 0, 2, 0, 1, 1);
+        gl.glColor3f(0.0f, 1.0f, 0.0f);
+        createShape(gl, 0, -2, 0, 1, -1);
 
-gl.glBegin(GL.GL_TRIANGLE_FAN);
-gl.glColor3f(1.0f,0.0f,0.0f);
-createShape(gl,0,2,0,1,1);
-gl.glColor3f(0.0f,1.0f,0.0f);
-createShape(gl,0,-2,0,1,-1);
-  
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.0f, 0.0f, 1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glVertex3f(1.0f, -1.0f, 1.0f);
+        gl.glEnd();
+
+        gl.glTranslatef(0.0f, 0.0f, -3.0f); //przesuniêcie o 6 jednostek
+        gl.glBegin(GL.GL_TRIANGLES);
+
+        float[] scianka1 = {-1.0f, -1.0f, 1.0f, //wpó?rz?dne pierwszego punktu
+            1.0f, -1.0f, 1.0f, //wspó?rz?dne drugiego punktu
+            0.0f, 1.0f, 0.0f}; //wspó?rz?dne trzeciego punktu
+        float[] normalna1 = WyznaczNormalna(scianka1, 0, 3, 6);
+        gl.glColor3f(1.0f, 0.0f, 1.0f);
+        gl.glNormal3fv(normalna1, 0);
+        gl.glVertex3fv(scianka1, 0);
+        gl.glVertex3fv(scianka1, 3);
+        gl.glVertex3fv(scianka1, 6);
+
+        float[] scianka2 = {1.0f, -1.0f, -1.0f, //wpó?rz?dne pierwszego punktu
+            -1.0f, -1.0f, -1.0f, //wspó?rz?dne drugiego punktu
+            0.0f, 1.0f, 0.0f}; //wspó?rz?dne trzeciego punktu
+        float[] normalna2 = WyznaczNormalna(scianka2, 0, 3, 6);
+
+        gl.glNormal3fv(normalna2, 0);
+        gl.glVertex3fv(scianka2, 0); //wspó?rz?dne 1-go punktu zaczynaj? si? od indeksu 0
+        gl.glVertex3fv(scianka2, 3); //wspó?rz?dne 2-go punktu zaczynaj? si? od indeksu 3
+        gl.glVertex3fv(scianka2, 6); //wspó?rz?dne 3-go punktu zaczynaj? si? od indeksu 6
+
+        float[] scianka3 = {-1.0f, -1.0f, -1.0f, //wpó?rz?dne pierwszego punktu
+            -1.0f, -1.0f, 1.0f, //wspó?rz?dne drugiego punktu
+            0.0f, 1.0f, 0.0f}; //wspó?rz?dne trzeciego punktu
+        float[] normalna3 = WyznaczNormalna(scianka3, 0, 3, 6);
+
+        gl.glNormal3fv(normalna3, 0);
+        gl.glVertex3fv(scianka3, 0); //wspó?rz?dne 1-go punktu zaczynaj? si? od indeksu 0
+        gl.glVertex3fv(scianka3, 3); //wspó?rz?dne 2-go punktu zaczynaj? si? od indeksu 3
+        gl.glVertex3fv(scianka3, 6); //wspó?rz?dne 3-go punktu zaczynaj? si? od indeksu 6
+
+        float[] scianka4 = {1.0f, -1.0f, 1.0f,
+            1.0f, -1.0f, -1.0f, //wspó?rz?dne drugiego punktu
+            0.0f, 1.0f, 0.0f}; //wspó?rz?dne trzeciego punktu
+        float[] normalna4 = WyznaczNormalna(scianka4, 0, 3, 6);
+
+        gl.glNormal3fv(normalna4, 0);
+        gl.glVertex3fv(scianka4, 0); //wspó?rz?dne 1-go punktu zaczynaj? si? od indeksu 0
+        gl.glVertex3fv(scianka4, 3); //wspó?rz?dne 2-go punktu zaczynaj? si? od indeksu 3
+        gl.glVertex3fv(scianka4, 6); //wspó?rz?dne 3-go punktu zaczynaj? si? od indeksu 6
+
         gl.glEnd();
 
         //Wykonanie wszystkich operacji znajduj¹cych siê w buforze
